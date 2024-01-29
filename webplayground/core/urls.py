7a149eb3587571e8e6ff473277fd60badec4c526
Luis-2102 <lalomoto5990@utm.edu.ec>
2024-01-28 21:40:32 -0500

    proyecto añadido1

--- webplayground/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HomePageView, SamplePageView
-from . import views
-
-urlpatterns = [
-    path('', HomePageView.as_view(), name="home"),
-    path('sample/', SamplePageView.as_view(), name="sample"),
-]
--- webplayground/vehiculo/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RentsConfig(AppConfig):
-    name = 'vehiculo'
-    verbose_name = "Gestor de rentas vehiculares"
